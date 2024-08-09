73c77db4db9a46428cfeff68cbeb4bd94b05280a
Mario D'Andrea <goffredo2004@gmail.com>
2024-08-09 11:50:24 +0200

    day 8, 2015

---
+++ 2015/8.py
@@ -0,0 +1,29 @@
+ESC, QUOTE = ord("\\"), ord("\"")
+
+with open("input", "rb") as input:
+    input = input.readlines()
+
+    literal = memory = 0
+    for string in input:
+        literal += len(string) + 2
+        i = 0
+        while i < len(string):
+            if string[i] == ESC:
+                if string[i + 1] in [ESC, QUOTE]:
+                    i += 1
+                else: # "\x00"
+                    i += 3
+            i += 1
+            memory += 1
+    print(literal - memory)
+
+    encoded = literal = 0
+    for string in input:
+        literal += len(string)
+        encoded += 2
+        for c in string:
+            if c in [ESC, QUOTE]:
+                encoded += 2
+            else:
+                encoded += 1
+    print(encoded - literal)
