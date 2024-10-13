41219836efa44d6b2702d486b9e01e38f6d255bd
Mario D'Andrea <goffredo2004@gmail.com>
2024-10-13 12:54:36 +0200

    Compile re

--- 2023/01.py
+++ 2023/01.py
@@ -12,10 +12,12 @@ with open("input") as input:
     def to_digit(s):
         return str(D.index(s) + 1) if s in D else s
 
-    total = 0
-    for line in lines:
-        # A few lines have overlapping words, e.g. 'eightwo',
-        # that's what the positive lookahead '?=' is for
-        d = [*map(to_digit, re.findall(rf"(?=({r'|'.join(D)}|\d))", line))]
-        total += int(d[0] + d[-1])
-    print(total)
+    # A few lines have overlapping words, e.g. 'eightwo',
+    # that's what the positive lookahead '?=' is for
+    N = re.compile(rf"(?=({'|'.join(D)}|\d))")
+    print(
+        sum(
+            int((d := [*map(to_digit, re.findall(N, line))])[0] + d[-1])
+            for line in lines
+        )
+    )
