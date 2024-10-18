9e9367d61250ec9b4a3eac5cbecf79e54a8daac8
Mario D'Andrea <goffredo2004@gmail.com>
2024-10-18 12:01:26 +0200

    Rewrote day 2, 2022

--- 2022/02.py
+++ 2022/02.py
@@ -1,72 +1,23 @@
-def firstPart():
-    file = open('data/day2.txt')
-    myScore = 0
+SHAPES = {"X": 0, "Y": 1, "Z": 2, "A": 0, "B": 1, "C": 2}
+with open("input") as tournament:
+    score_p1 = score_p2 = 0
+    for round in tournament:
+        opp, me = round.split()
+        score_p1 += SHAPES[me] + 1
+        if SHAPES[opp] == SHAPES[me]:
+            score_p1 += 3
+        elif (SHAPES[opp] + 1) % 3 == SHAPES[me]:
+            score_p1 += 6
 
-    # A: rock
-    # B: paper
-    # C: scissors
+        # part two
+        if me == "X":
+            score_p2 += (SHAPES[opp] - 1) % 3
+            print((SHAPES[opp] - 1) % 3 + 1)
+        elif me == "Y":
+            score_p2 += SHAPES[opp] + 3
+        elif me == "Z":
+            score_p2 += (SHAPES[opp] + 1) % 3 + 6
+        score_p2 += 1  # index -> weight
 
-    # X: rock
-    # Y: paper
-    # Z: scissors
-
-    for line in file:
-        if line[0] == 'A' and line[2] == 'Y' or \
-             line[0] == 'B' and line[2] == 'Z' or \
-             line[0] == 'C' and line[2] == 'X':
-            myScore += 6
-        elif line[0] == 'A' and line[2] == 'X' or \
-                 line[0] == 'B' and line[2] == 'Y' or \
-                 line[0] == 'C' and line[2] == 'Z':
-            myScore += 3
-
-        if line[2] == 'X' : myScore += 1
-        if line[2] == 'Y' : myScore += 2
-        if line[2] == 'Z' : myScore += 3
-
-    print(myScore)
-
-def secondPart():
-    file = open('day2.txt')
-    myScore = 0
-
-    # A: rock
-    # B: paper
-    # C: scissors
-
-    # X: lose
-    # Y: draw
-    # Z: win
-
-
-    for line in file:
-        op = line[0]
-        out = line[2]
-
-        if out == 'X':
-            if op == 'A':
-                myScore += 3
-            elif op == 'B':
-                myScore += 1
-            elif op == 'C':
-                myScore += 2
-
-        if out == 'Y':
-            if op == 'A':
-                myScore += 1 + 3
-            elif op == 'B':
-                myScore += 2 + 3
-            elif op == 'C':
-                myScore += 3 + 3
-
-        if out == 'Z':
-            if op == 'A':
-                myScore += 2 + 6
-            elif op == 'B':
-                myScore += 3 + 6
-            elif op == 'C':
-                myScore += 1 + 6
-
-    print(myScore)
-
-secondPart()
+    print(score_p1)
+    print(score_p2)
