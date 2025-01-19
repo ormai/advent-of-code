43c202623bdb50fdf01627eeae0b9408252a5908
Mario D'Andrea <goffredo2004@gmail.com>
2025-01-19 10:38:20 +0100

    day 1, 2020

---
+++ 2020/01.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var buf: [1024]u8 = undefined;
+    const input = try std.fs.cwd().readFile("input", &buf);
+    var nums: [200]u32 = undefined;
+    var index: usize = 0;
+    var it = std.mem.tokenizeScalar(u8, input, '\n');
+    while (it.next()) |s| : (index += 1) {
+        nums[index] = try std.fmt.parseInt(u32, s, 10);
+    }
+
+    for (nums[0..], 0..) |a, i| {
+        for (nums[i + 1 ..], 0..) |b, j| {
+            if (a + b == 2020) {
+                std.debug.print("Part one: {d}\n", .{a * b});
+            }
+            for (nums[i + j + 1 ..]) |c| {
+                if (a + b + c == 2020) {
+                    std.debug.print("Part two: {d}\n", .{a * b * c});
+                }
+            }
+        }
+    }
+}
