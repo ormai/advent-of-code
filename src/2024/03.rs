c3ca0ef53e892200e46f6e62b3f3fdcc842339f5
Mario D'Andrea <goffredo2004@gmail.com>
2025-12-22 16:03:40 +0100

    declare regexes as global statics with LazyLock
    Discovered here: https://notgull.net/block-pattern/

--- src/2024/03.rs
+++ src/2024/03.rs
@@ -1,29 +1,32 @@
 use regex::{Captures, Regex};
+use std::sync::LazyLock;
 
 aoc::solution!(2024, 3);
 
+static MUL: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("Mul regex should be valid"));
+
+static COMMAND: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"(don't\(\)|do\(\))").expect("Command regex should be valid"));
+
 pub fn part_one(input: &str) -> Option<u64> {
-    let mul = mul()?;
     Some(
         input
             .lines()
-            .map(|line| mul.captures_iter(line).map(product).sum::<u64>())
+            .map(|line| MUL.captures_iter(line).map(product).sum::<u64>())
             .sum(),
     )
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mul = mul()?;
-    let command = command()?;
-
     let mut acc = 0;
     let mut enabled = true;
     for line in input.lines() {
         let mut prev_match_end = 0;
-        for captures in mul.captures_iter(line) {
+        for captures in MUL.captures_iter(line) {
             let range = captures.get(0)?.range();
 
-            if let Some(command_match) = command.captures(&line[prev_match_end..range.start]) {
+            if let Some(command_match) = COMMAND.captures(&line[prev_match_end..range.start]) {
                 enabled = command_match.get(0)?.as_str() == "do()";
             }
             if enabled {
@@ -33,14 +36,6 @@ pub fn part_two(input: &str) -> Option<u64> {
         }
     }
     Some(acc)
-}
-
-fn mul() -> Option<Regex> {
-    Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").ok()
-}
-
-fn command() -> Option<Regex> {
-    Regex::new(r"(don't\(\)|do\(\))").ok()
 }
 
 fn product(captures: Captures) -> u64 {
